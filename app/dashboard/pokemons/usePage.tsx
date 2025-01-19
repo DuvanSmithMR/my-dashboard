3884ce16243773ccc4277b3e41b8de9e377b0e80
duvan smith <dsmith.mr@gmail.com>
2025-01-19 12:58:45 -0500

    Se agrega tipado

--- app/dashboard/pokemons/usePage.tsx
+++ app/dashboard/pokemons/usePage.tsx
@@ -1,9 +1,19 @@
-const getPokemons = async (limit = 10, offset = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "./models";
+
+const getPokemons = async (
+  limit = 10,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((res) => res.json());
 
-  return data;
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
 
 export const usePage = async () => {
